7b9d92707b201b3a446befbf7919bac83a75e728
FIRST_NAME LAST_NAME <dineshreddy1590@gmail.com>
2022-12-19 14:53:37 +0530

    new

--- src/main/java/com/myproject/flames/controller/FlamesController.java
+++ src/main/java/com/myproject/flames/controller/FlamesController.java
@@ -6,10 +6,7 @@ import com.myproject.flames.service.FlamesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @CrossOrigin("http://localhost:4200")
@@ -23,5 +20,11 @@ public class FlamesController {
     public ResponseEntity<Response> flames(@RequestBody FlamesModel flamesModel) {
 
       return new ResponseEntity<>(flamesService.flames(flamesModel), HttpStatus.OK);
+    }
+
+
+    @GetMapping("/home")
+    public ResponseEntity<String> get() {
+        return new ResponseEntity<>("hello world",HttpStatus.OK);
     }
 }
